df89dde1af0b89069bbcb5cec8f40ca84682e18b
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-20 23:37:20 +0600

    [index]: `Indexer` w filename and sanitization;

--- local-search/Indexer.py
+++ local-search/Indexer.py
@@ -1,11 +1,29 @@
 from sentence_transformers import SentenceTransformer
+from FileParser import FileParser
 
 class Indexer():
-    def __init__(self, content, model_name):
-        self.content = content
+    sanitize_key = "__meaw__woof__"
+
+    def __init__(self, file_path, model_name = 'sentence-transformers/all-MiniLM-L6-v2'):
+        self.file_path = file_path
         self.model_name = model_name
 
-    def index(self):
+    @staticmethod
+    def sanitize_key(key):
+        return key.replace("/", Indexer.sanitize_key)
+
+    @staticmethod
+    def desanitize_key(key):
+        return key.replace(Indexer.sanitize_key, "/")
+
+    def embed(self, content):
         model = SentenceTransformer(self.model_name)
-        embeddings = model.encode(self.content, convert_to_tensor=True)
+        embeddings = model.encode(content)
         return embeddings
+    
+    def index(self, db):
+        content = FileParser(self.file_path).parse()
+        embeddings = self.embed(content)
+        model_name_sanitized = Indexer.sanitize_key(self.model_name)
+        sample_document = {"location": self.file_path, model_name_sanitized: embeddings}
+        db.insert_document(sample_document)
