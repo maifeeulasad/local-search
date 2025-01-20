99dd6bcd777d218f8510df5cc58476b1ae2d33ac
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-21 00:27:13 +0600

    [refactor]: only pass db instance where being used;

--- local-search/Retrieval.py
+++ local-search/Retrieval.py
@@ -3,15 +3,14 @@ from Indexer import Indexer
 
 class Retrieval():
     def __init__(self, db, model_name, threshold=0.8, keep_attribute=["location"]):
-        self.db = db
         db_model_name = Indexer.desanitize_key(model_name)
         self.db_model_name = db_model_name
         self.threshold = threshold
         self.keep_attribute = keep_attribute
 
-    def search(self, query_embedding, top_k=None):
+    def search(self, db, query_embedding, top_k=None):
         projection = {attribute: 1 for attribute in self.keep_attribute}
-        documents = self.db.read_documents({self.db_model_name: {"$exists": True}}, projection)
+        documents = db.read_documents({self.db_model_name: {"$exists": True}}, projection)
         embeddings = [doc[self.db_model_name] for doc in documents]
         hits = util.semantic_search(query_embedding, embeddings, top_k=top_k)
         results = [(doc["location"], hit["score"]) for doc, hit in zip(documents, hits[0])]
