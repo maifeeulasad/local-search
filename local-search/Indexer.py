33488dd99748159611c7c2aa9242a9b9fe27cb0f
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-20 18:23:57 +0600

    [index]: implemented `Indexer` w `sentence_transformers`;

---
+++ local-search/Indexer.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
+
+class Indexer():
+    def __init__(self, content, model_name):
+        self.content = content
+        self.model_name = model_name
+
+    def index(self):
+        model = SentenceTransformer(self.model_name)
+        embeddings = model.encode(self.content, convert_to_tensor=True)
+        return embeddings
