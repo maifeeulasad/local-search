cc37842b6ed1768bfe3749beb94b0887fd6d7898
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-21 00:19:06 +0600

    [refactor]: variable naming;

--- local-search/Indexer.py
+++ local-search/Indexer.py
@@ -2,7 +2,7 @@ from sentence_transformers import SentenceTransformer
 from FileParser import FileParser
 
 class Indexer():
-    sanitize_key = "__meaw__woof__"
+    sanitize_key_value = "__meaw__woof__"
 
     def __init__(self, file_path, model_name = 'sentence-transformers/all-MiniLM-L6-v2'):
         self.file_path = file_path
@@ -10,20 +10,20 @@ class Indexer():
 
     @staticmethod
     def sanitize_key(key):
-        return key.replace("/", Indexer.sanitize_key)
+        return key.replace("/", Indexer.sanitize_key_value)
 
     @staticmethod
     def desanitize_key(key):
-        return key.replace(Indexer.sanitize_key, "/")
+        return key.replace(Indexer.sanitize_key_value, "/")
 
     def embed(self, content):
         model = SentenceTransformer(self.model_name)
-        embeddings = model.encode(content)
-        return embeddings
+        embedding = model.encode(content)
+        return embedding
     
     def index(self, db):
         content = FileParser(self.file_path).parse()
-        embeddings = self.embed(content)
+        embedding = self.embed(content)
         model_name_sanitized = Indexer.sanitize_key(self.model_name)
-        sample_document = {"location": self.file_path, model_name_sanitized: embeddings}
+        sample_document = {"location": self.file_path, model_name_sanitized: embedding.tolist()}
         db.insert_document(sample_document)
